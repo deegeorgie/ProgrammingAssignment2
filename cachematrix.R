6db7072be82bc0a129503d6ec306a3467e3f98e5
deegeorgie <deebodiong@gmail.com>
2014-12-20 09:51:25 +0100

    first edit of function
    function to calculate inverse of matrix or retrieve it from cache if it
    already exists.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,69 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function is meant to load the inverse of a matrix from cache
+## or compute it from scratch if it has not yet been calculated...
 
-## Write a short comment describing this function
+## special matrix function that can cache its' inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        
+        ## set the value of the matrix
+        
+        i<- NULL
+        
+        set <- function(y) {
+                
+                x <<- y
+                
+                i <<- NULL
+        }
+        ## get the value of the matrix
+        
+        get <- function() x
+        
+        ## set the inverse of the matrix
+        
+        setinverse <- function(solve) i <<- solve
+        
+        getinverse <- function() i
+        
+        ## get the inverse of the matrix
+        
+        list(set = set, get = get,
+             
+             setinverse = setinverse,
+             
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## function to return the inverse of the matrix from the 
+## makeCacheMatrix function. If the inverse exists in cache,
+## retrieve it.
 
 cacheSolve <- function(x, ...) {
+        
         ## Return a matrix that is the inverse of 'x'
+        
+        ## get the inverse of the matrix  
+        
+        i <- x$getinverse()
+        
+        ## check if there is the matrix 
+        
+        if(!is.null(i)) {
+                
+                message("retrieving cached data")
+                
+                return(i)
+        }
+        ## if not: get the inverse of the matrix   
+        
+        data <- x$get()
+        
+        i <- solve(data, ...)
+        
+        ## set the inverse of the matrix 
+        
+        x$setinverse(i)
+        
+        i
 }
